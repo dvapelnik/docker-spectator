9feddf8a6752f878c9af7e659ca8eb4671a57b7e
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-09-25 15:22:43 +0300

    Save data only 1 hour by default

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ root_path = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 config = {
     'db.filename': root_path + 'db.bin',
-    'db.limit.hours': 24,
+    'db.limit.hours': 1,
     'docker.spectator': root_path + 'docker-spectator.py',
     'docker.spectator.log': root_path + 'log/docker-spectator.log',
     'daemon.pid': root_path + 'run/docker-spectator-daemon.pid',
